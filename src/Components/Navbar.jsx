001359dfbbd3c15c10415983694117b25eb51a23
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-07-13 13:50:35 +0600

    create the sub menu items & use tailwind group functionality

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,8 +17,12 @@ const Navbar = () => {
           <div className="relative flex h-full items-center p-4 cursor-pointer font-bold text-pink-200 hover:text-zinc-200 hover:bg-white/10 transition-colors ease-in-out">
             <span>Line-Up</span>
           </div>
-          <div className="relative flex h-full items-center p-4 cursor-pointer font-bold text-pink-200 hover:text-zinc-200 hover:bg-white/10 transition-colors ease-in-out">
+          <div className="relative flex h-full items-center p-4 cursor-pointer font-bold text-pink-200 hover:text-zinc-200 hover:bg-white/10 transition-colors ease-in-out group">
             <span>Tickets</span>
+            <div className="group-hover:block  hidden absolute top-full bg-pink-500 right-0 whitespace-nowrap rounded-b-md">
+              <h1 className="p-4 font-bold hover:bg-white/5 hover:text-zinc-200 transition-colors ease-in-out cursor-pointer text-pink-200 text-end">one day tickets</h1>
+              <h1 className="p-4 font-bold hover:bg-white/5 hover:text-zinc-200 transition-colors ease-in-out cursor-pointer text-pink-200 text-end">seven days tickets</h1>
+            </div>
           
            
          
