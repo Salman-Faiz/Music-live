6278c4278ee801dada3f95704b5b175b7c0f4cbd
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-07-13 16:53:12 +0600

    implement mobile submenu using react usestate

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 const Navbar = () => {
   const [isToggled, setIsToggoled] =useState(false)
+  const [showTickets, setShowTickets] = useState(false);
+
   const handleToggleMenu = ()=>{
     setIsToggoled(!isToggled)
 
+  }
+  const handleTickets = ()=>{
+    setShowTickets(!showTickets);
+    
   }
   return (
     <div>
@@ -28,9 +34,9 @@ const Navbar = () => {
             </div>  
           }
 
-          
+          {/* show menu items for mobile */}
         {
-            isToggled && <div className="absolute right-0 top-[62px] text-center  w-full    rounded-b-md space-y-3 text-xl py-3 bg-gradient-to-r from-rose-500 to-pink-500">
+            isToggled && <div className="absolute right-0 top-[62px]   w-full    rounded-b-md space-y-3 text-xl py-3 bg-gradient-to-r from-rose-500 to-pink-500">
              
           <div className="relative flex h-full items-center p-4 cursor-pointer font-bold text-pink-200 hover:text-zinc-200 hover:bg-white/10 transition-colors ease-in-out">
             <span>Home</span>
@@ -39,13 +45,25 @@ const Navbar = () => {
           <div className="relative flex h-full items-center p-4 cursor-pointer font-bold text-pink-200 hover:text-zinc-200 hover:bg-white/10 transition-colors ease-in-out">
             <span>Line-Up</span>
           </div>
-          <div className="relative flex h-full items-center p-4 cursor-pointer font-bold text-pink-200 hover:text-zinc-200 hover:bg-white/10 transition-colors ease-in-out group">
-            <span>Tickets</span>
-            <div className="group-hover:block  hidden absolute top-full bg-pink-500 bottom-0 whitespace-nowrap rounded-b-md">
-              <h1 className="p-4 font-bold hover:bg-white/5 hover:text-zinc-200 transition-colors ease-in-out cursor-pointer text-pink-200 text-start">one day tickets</h1>
-              <h1 className="p-4 font-bold hover:bg-white/5 hover:text-zinc-200 transition-colors ease-in-out cursor-pointer text-pink-200 text-start">seven days tickets</h1>
-            </div>
+
+
+          <div className="group relative  h-full i cursor-pointer  text-pink-200 hover:text-zinc-200 hover:bg-white/10 transition-colors ease-in-out">
+            <div onClick={handleTickets} className="p-4  font-bold">Tickets</div>
+           
+           {
+            showTickets ? <div className="relative">
+            <div className=" text-pink-200 hover:text-zinc-200 hover:bg-white/5 transition-colors ease-in-out p-4">
+               <span>One Day Tickets</span>
+             </div>
+             <div className=" text-pink-200 hover:text-zinc-200 hover:bg-white/5 transition-colors ease-in-out p-4">
+               <span>Seven Day Tickets</span>
+             </div>
+            </div> : ''
+           }
           </div>
+
+
+
           <div className="relative flex h-full items-center p-4 cursor-pointer font-bold text-pink-200 hover:text-zinc-200 hover:bg-white/10 transition-colors ease-in-out">
             <span>Support</span>
           </div>
@@ -54,7 +72,7 @@ const Navbar = () => {
         </div>
 
 
-        {/* menu items */}
+        {/* menu items for lerger device*/}
         <div className="hidden md:flex flex-1 items-center justify-end">
 
           <div className="relative flex h-full items-center p-4 cursor-pointer font-bold text-pink-200 hover:text-zinc-200 hover:bg-white/10 transition-colors ease-in-out">
